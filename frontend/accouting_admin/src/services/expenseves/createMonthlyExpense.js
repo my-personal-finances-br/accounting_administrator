d5b35642dcadae5fa0244319244e26b652a35efb
Lucas Honda <eliashondalucas@hotmail.com>
2023-12-11 15:34:39 -0300

    fix

--- frontend/accouting_admin/src/services/expenseves/createMonthlyExpense.js
+++ frontend/accouting_admin/src/services/expenseves/createMonthlyExpense.js
@@ -15,7 +15,7 @@ const getCookieValue = (name) => {
 
   const createMonthlyExpense = (data) => {
     const response = axios.post(
-        `http://localhost:8000/api/internal/expenses/monthly_expense`,
+        `http://localhost:8000/api/internal/monthly_expense`,
         {month: "default", month_number: 0},
         {
             withCredentials: true,
