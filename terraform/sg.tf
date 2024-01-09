4339c867107fc75a2bc977bc71ecaf56f47954c3
Igor araujo de Sousa <iasousa.platform@rd.com.br>
2024-01-08 22:36:25 -0300

    Add sg group outbound

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -12,6 +12,13 @@ resource "aws_security_group" "instance_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
     Name = "instance-sg"
