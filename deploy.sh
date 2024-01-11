d15c92572f77bc54fc8f506d9b780d9d5df85f0e
Igor Araujo de Sousa <igotaraujo97@gmail.com>
2024-01-11 00:46:48 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 
 ls 
 cat docker-compose.yml
-cd accounting_administrator
-git pull https://${{ secrets.GIT_USER }}:${{ secrets.GIT_TOKEN }}@github.com/my-personal-finances-br/accounting_administrator.git
 cd .. && docker-compose up --build
