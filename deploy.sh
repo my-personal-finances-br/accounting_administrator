23a2ec196caa1a69b5eb6271bb143a568c2ab5d7
Igor Araujo de Sousa <igotaraujo97@gmail.com>
2024-01-11 01:01:31 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd ..
-ls 
-cat docker-compose.yml
-docker compose up --build
+mv ../docker-compose-prod.yml .
+cat docker-compose-prod.yml
+docker compose -f docker-compose-prod.yml up --build
